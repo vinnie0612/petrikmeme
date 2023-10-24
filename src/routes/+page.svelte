12cf0fea56ff2a84974096a2b841c9dbc062602a
Tamás Vince Kornél <tamas.vince.kornel@petrik.hu>
2023-10-24 22:35:58 +0200

    also the title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,10 @@
   import PostCard from '$lib/component/PostCard.svelte';
 </script>
 
+<svelte:head>
+    <title>PetrikMeme</title> 
+</svelte:head>
+
 <main class="">
   <div class="max-w-3xl grid space-y-4 p-4 mx-auto">
     {#each data.result as post}
