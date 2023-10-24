4c6b6b652e3d31fd0b4d8ecba5315863bc7b761f
Tamás Vince Kornél <tamas.vince.kornel@petrik.hu>
2023-10-24 22:35:20 +0200

    favicons and such

--- src/lib/component/Navbar.svelte
+++ src/lib/component/Navbar.svelte
@@ -2,8 +2,9 @@
   import { currentUser, signIn, signOut } from '$lib/util/pocketbase';
 </script>
 
-<nav class="bg-slate-800 py-4 px-3 justify-between flex items-center">
-  <a class="text-2xl font-bold" href="/">Petrikmeme:3</a>
+<nav class="bg-slate-800 py-2 px-3 justify-between flex items-center">
+  <a class="w-10 font-bold" href="/">
+  <img src="/favicon.png" alt=""></a>
   {#if $currentUser}
     <button on:click={signOut} class="bg-slate-600 hover:bg-slate-700 transition p-2 rounded-lg"
       >Kilépés</button
