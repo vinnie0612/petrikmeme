9504a2cca5df5f40d49f38bbe9432f743e229e8f
Susa Milan <mailtosnek@protonmail.com>
2023-10-25 08:58:11 +0200

    added getPosts() (paginated)

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,16 +1,6 @@
+import { getPosts } from '$lib/types/post';
 import { pb } from '$lib/util/pocketbase';
 
 export const load = async () => {
-  const getPosts = async () => {
-    const posts = await pb.collection('posts').getList(1, 50, { requestKey: null });
-    for (const post of posts.items) {
-      if (post.upvotes && post.downvotes) {
-        post.score = post.upvotes.length - post.downvotes.length;
-      }
-      post.image = pb.files.getUrl(post, post.image);
-    }
-    return posts.items;
-  };
-
-  return { result: getPosts() };
+  return { result: await getPosts(1, 50, '', '-created') };
 };
